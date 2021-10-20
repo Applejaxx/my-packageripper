d5d65afcc3bdcb4647c9cad3f89c3eb06da8413d
Applejaxx <92328693+Applejaxx@users.noreply.github.com>
2021-10-20 00:10:47 -0400

    C:\Users\Mercedes\Instarp\my-package-exe2off
    1ee6cf0baea3174ea88153ea559d26a902b0169b
    C:\Users\Mercedes\Instarp\my-package-exe2off

---
+++ spec/my-packageripper-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import MyPackageripperView from '../lib/my-packageripper-view';
+
+describe('MyPackageripperView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
